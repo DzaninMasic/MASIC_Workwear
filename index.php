bc8714c00b6c855b26fb0266c16590ccb8f0e504
DzaninMasic <dzanin.masic@stu.ibu.edu.ba>
2022-03-20 00:43:43 +0100

    useless commit

--- index.php
+++ index.php
@@ -5,8 +5,24 @@ error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
 require_once 'rest/dao/ProjectDao.class.php';
+require_once 'rest/dao/AdminsDao.class.php';
+require_once 'rest/dao/ButtonsDao.class.php';
+require_once 'rest/dao/ThreadsDao.class.php';
+require_once 'rest/dao/VelcroDao.class.php';
+
 Flight::route('/', function(){
   $dao = new ProjectDao();
+  $admins = new AdminsDao();
+  $buttons = new ButtonsDao();
+  $threads = new ThreadsDao();
+  $velcro = new VelcroDao();
+  $velcro->add($_REQUEST['name'], $_REQUEST['color'], $_REQUEST['type'], $_REQUEST['length'], $_REQUEST['available']);
+  die;
+  //$threads->add($_REQUEST['code'], $_REQUEST['name'], $_REQUEST['color'], $_REQUEST['length'], $_REQUEST['amount'], $_REQUEST['width'], $_REQUEST['available']);
+  $getThreads=$threads->get_all_threads();
+  print_r($getThreads);
+  die;
+
   $op = $_REQUEST['op'];
 
   switch ($op) {
--- rest/dao/AdminsDao.class.php
+++ rest/dao/AdminsDao.class.php
@@ -1,42 +1,32 @@
-
 <?php
 
-class AdminsDao{
+require_once 'BaseDao.class.php';
 
-  private $conn;
-
-  //CONSTRUCTOR
-  public function __construct(){
-  $servername = "sql11.freemysqlhosting.net";
-  $username = "sql11479687";
-  $password = "vA323dsrUN";
-  $schema = "sql11479687";
-  $this->conn = new PDO("mysql:host=$servername;port=3306;dbname=$schema", $username, $password);
-  // set the PDO error mode to exception
-  $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
+class ButtonsDao extends BaseDao{
   //GET ALL
-  public function get_all(){
-    $stmt = $this->conn->prepare("SELECT * FROM admins");
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  public function get_all_buttons(){
+    return $this->query("SELECT * FROM buttons");
+  }
+  //GET BY NAME
+  public function get_button_by_name($name){
+    return $this->queryUnique("SELECT * FROM buttons WHERE name=:name",["name"=>$name]);
   }
   //ADD TO DATABASE
-  public function add($name, $email, $password){
-    $stmt = $this->conn->prepare("INSERT INTO admins (name, email, password) VALUES (:name, :email, :password)");
-    $stmt->execute(['name' => $name, 'email' => $email,'password' => $password]);
+  public function add($name, $amount, $size, $type, $available){
+    $stmt = $this->conn->prepare("INSERT INTO buttons (name, amount, size, type, available) VALUES (:name, :amount, :size, :type, :available)");
+    $stmt->execute(['name' => $name, 'amount' => $amount,'size' => $size, 'type'=>$type, 'available' => $available]);
   }
   //DELETE (kinda useless)
   public function delete($id){
-    $stmt = $this->conn->prepare("DELETE FROM admins WHERE id=:id");
+    $stmt = $this->conn->prepare("DELETE FROM buttons WHERE id=:id");
     $stmt->bindParam(':id', $id); // SQL injection prevention
     $stmt->execute();
   }
   //UPDATE
-  public function update($id, $name, $color, $length, $available){
-    $stmt = $this->conn->prepare("UPDATE admins SET name=:name, email=:email, password=:password WHERE id=:id");
-    $stmt->execute(['id' => $id, 'name' => $name, 'email' => $email, 'password'=>$password]);
+  public function update($id, $name, $amount, $size, $type, $available){
+    $stmt = $this->conn->prepare("UPDATE buttons SET name=:name, amount=:amount, size=:size, type=:type, available=:available WHERE id=:id");
+    $stmt->execute(['id' => $id, 'name' => $name, 'amount' => $amount, 'size'=>$size, 'type'=>$type, 'available'=>$available]);
   }
 }
 
-?>
+ ?>
