44a9d9aec02be367b806d694cc8b2110a2e9fa10
DzaninMasic <dzanin.masic@stu.ibu.edu.ba>
2022-04-07 13:35:37 +0200

    Commit 2

---
+++ rest/services/index.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'C:\xampp\htdocs\MASIC_Workwear\vendor\autoload.php';
+require_once 'C:\xampp\htdocs\MASIC_Workwear\rest\dao\ProjectDao.class.php';
+Flight::register('projectDao', 'ProjectDao');
+Flight::route('/print', function(){
+  echo "This is Dzanin \n";
+});
+//GET ALL
+Flight::route('GET /material', function(){
+Flight::json(Flight::projectDao()->get_all());
+});
+//GET INDIVIDUAL BY NAME
+Flight::route('GET /material/@id', function($id){
+  Flight::json(Flight::projectDao()->get_by_id($id));
+});
+//ADD
+Flight::route('POST /material', function(){
+  Flight::json(Flight::projectDao()->add(Flight::request()->data->getData()));
+});
+//UPDATE
+Flight::route('PUT /material/@id', function($id){
+  $data = Flight::request()->data->getData();
+  $data['id'] = $id;
+  Flight::json(Flight::projectDao()->update($data));
+});
+//DELETE
+Flight::route('DELETE /material/@id', function($id){
+  Flight::projectDao()->delete($id);
+  Flight::json(["message" => "deleted"]);
+});
+
+Flight::start();
+ ?>
