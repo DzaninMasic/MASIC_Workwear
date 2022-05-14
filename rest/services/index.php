e25e11105b6d2ada8bcf99b87d3cd1d97172a7b3
DzaninMasic <dzanin.masic@stu.ibu.edu.ba>
2022-05-14 21:42:55 +0200

    Login middleware partially added

--- rest/services/index.php
+++ rest/services/index.php
@@ -3,6 +3,9 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
 
+  use Firebase\JWT\JWT;
+  use Firebase\JWT\key;
+
   require_once 'C:\xampp\htdocs\MASIC_Workwear\vendor\autoload.php';
   require_once 'C:\xampp\htdocs\MASIC_Workwear\rest\services\MaterialService.class.php';
   require_once 'C:\xampp\htdocs\MASIC_Workwear\rest\services\ColorsService.class.php';
@@ -12,6 +15,31 @@
   Flight::register('materialService', 'MaterialService');
   Flight::register('colorsService', 'ColorsService');
   Flight::register('adminService', 'AdminService');
+
+  Flight::map('error', function(Exception $ex){
+    Flight::json(['message' => $ex->getMessage()],500);
+  });
+  //MIDDLEWARE
+  Flight::route('/*', function(){
+    //perform JWT decode
+    $path = Flight::request()->url;
+    if ($path == '/login') return TRUE; // exclude login route from middleware
+
+    $headers = getallheaders();
+    if (@!$headers['Authorization']){
+      Flight::json(["message" => "Authorization is missing"], 403);
+      return FALSE;
+    }else{
+      try {
+        $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+        Flight::set('user', $decoded);
+        return TRUE;
+      } catch (\Exception $e) {
+        Flight::json(["message" => "Authorization token is not valid"], 403);
+        return FALSE;
+      }
+    }
+  });
 
   require_once 'C:\xampp\htdocs\MASIC_Workwear\rest\routes\ColorRoutes.php';
   require_once 'C:\xampp\htdocs\MASIC_Workwear\rest\routes\MaterialRoutes.php';
