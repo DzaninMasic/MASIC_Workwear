3c6726d5ed66b4f6f11019da503d0863e5ca993e
DzaninMasic <dzanin.masic@stu.ibu.edu.ba>
2022-06-19 19:25:15 +0200

    Added color button

---
+++ js/color-service.js
@@ -0,0 +1,37 @@
+var ColorService={
+
+  init: function(){
+    $('#addColorForm').validate({
+      submitHandler:function(form){
+        var color = Object.fromEntries((new FormData(form)).entries());
+        ColorService.add(color);
+      }
+    });//JQUERY VALIDATION
+  },
+
+  add: function(color){
+    console.log("Nas log",color);
+    $.ajax({
+      url: 'rest/colors',
+      type: 'POST',
+      data: JSON.stringify(color),
+      contentType: 'application/json',
+      dataType: "json",
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+      },
+      success: function(result){
+        toastr.success("Color added!");
+        $("#exampleModal4").modal("hide");
+        $("#material-list").html('<div class="d-flex justify-content-center"><div class="spinner-border text-primary" style="width : 5rem ; height : 5rem;" role="status"> <span class="sr-only"></span>  </div></div>');
+        //console.log(data);
+        MaterialService.list();
+        console.log(result);
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        toastr.error(XMLHttpRequest.responseJSON.message);
+      }
+    });
+  }
+
+}
--- rest/services/ColorsService.class.php
+++ rest/services/ColorsService.class.php
@@ -7,7 +7,21 @@
     public function __construct(){
       parent::__construct(new ColorsDao());
     }
-    
+
+    public function add($entity){
+      try {
+        return $this->dao->add($entity);
+      } catch (\Exception $e) {
+        if (str_contains($e->getMessage(), 'SQLSTATE[23000]')) {
+            throw new Exception('Color with the same name exists.');
+        }
+        else {
+          throw $e;
+        }
+      }
+      //return $this->dao->add($entity);
+    }
+
     public function get_material_by_color_id($color_id){
       return $this->dao->get_material_by_color_id($color_id);
     }
